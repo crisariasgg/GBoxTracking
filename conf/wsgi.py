d1bd12c8ce59e39e0cd6417cb40fb167d8f0e5f0
crisariasgg <crisarias@grupoguticia.com>
2021-10-26 17:26:12 -0600

    conf wsgi added development and runtime

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -5,7 +5,11 @@ WSGI config for project.
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.development')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+

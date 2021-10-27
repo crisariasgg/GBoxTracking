f0896ee8d74eeefac19de09c3d2106dfd226e434
crisariasgg <crisarias@grupoguticia.com>
2021-10-27 08:09:50 -0600

    changed wsgi whitenoise configuration

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -5,11 +5,9 @@ WSGI config for project.
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.development')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
 

9be1361ef8a5b2fae6031c63249a73b4eaab7917
crisariasgg <crisarias@grupoguticia.com>
2021-10-27 14:59:41 -0600

    wsgi conf

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -6,7 +6,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.production')
 
 application = get_wsgi_application()
 

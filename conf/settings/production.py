f19f1f9ebcec8e010729c0ed5f5a65425a3eaca7
crisariasgg <crisarias@grupoguticia.com>
2021-10-27 17:11:39 -0600

    added new conf to database for production

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -35,10 +35,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-DATABASES = {
-    "default": {
-        dj_database_url.config(default=config('DATABASE_URL'))
-    }
-}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'),conn_max_age=600, ssl_require=True)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
